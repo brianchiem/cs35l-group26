2c5ad2bc63b1a09e942e2bc6f0273f83af517658
em568 <Emiliayuja@hotmail.com>
2024-06-03 21:19:19 -0700

    Navigation Bar now has a drop down option

--- wordle-clone/src/components/Navbar.jsx
+++ wordle-clone/src/components/Navbar.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import './styles/Navbar.css'
 import logo from './assets/w.png'
 import { Outlet, Link } from "react-router-dom";
-
+import {useState} from 'react';
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
 import pfp from '../uploads/defaultpfp.png'
+import { BiGridSmall } from "react-icons/bi";
 
 
 
 function Navbar() {
+    const [NavBarShow, setNavBarShow] = useState(false);
     const { logout } = useLogout()
     const { user } = useAuthContext()
     const handleClick = () => {
@@ -56,22 +58,6 @@ function Navbar() {
                             </div>
                         </div>
                     </Link>
-                    <div className="nav-buttons">
-                        <Link className="leaderboards" to="/Leaderboards">
-                            Leaderboards
-                        </Link>
-                        <Link className="social" to="/Social">
-                            Social
-                        </Link>
-                        {/* <Link className="howtoplay" to='/Game' >
-                            Game
-                        </Link> */}
-                        <Link className="navpfp" to={`/Profile`} >
-                            <img src={!user.profilepicture ? 'http://localhost:4000/images/defaultpfp.png' : 'http://localhost:4000/images/' + user.profilepicture}/>
-                            <div>{user.username}</div>
-                        </Link>
-                        <button className="logout" onClick={handleClick}>Log out</button>
-                    </div>
                     </>
                 )}
                 {!user && (
@@ -91,18 +77,54 @@ function Navbar() {
                             </div>
                         </div>
                     </Link>
-                    <div className="nav-buttons">
-                        <Link to="/login">
-                            Login
-                        </Link>
-                        <Link to="/signup">
-                            Signup
-                        </Link>
-                    </div>
                     </>
                 )}
-                
-            </div>
+            
+            
+            <button className="align-dot" onClick={() => setNavBarShow((prevState) => !prevState)}>
+                <BiGridSmall className="Grid-class"/>
+            </button>
+
+            { NavBarShow && (   
+                <>                
+                 <div className="asidebar-box">
+                    {user && (
+                        <div className="side-bar-container">
+                             <Link className="navpfp" to={`/Profile`} >
+                            <img src={!user.profilepicture ? 'http://localhost:4000/images/defaultpfp.png' : 'http://localhost:4000/images/' + user.profilepicture}/>
+                            <div className="font-final1" >{user.username}</div>
+                        </Link>
+                        <Link className="font-final1" to="/Leaderboards">
+                            Leaderboards
+                        </Link>
+                        <Link className="font-final1" to="/Social">
+                            Social
+                        </Link>
+                        <button className="log_out" onClick={handleClick}>Log out</button>
+                    </div>
+                    )}
+
+                    {!user && (
+                         <div className="side-bar-container">
+                             <Link className="font-final1" to="/login">
+                                 Login
+                             </Link>
+                             <Link className="font-final1" to="/signup">
+                                 Signup
+                             </Link>
+                             </div>
+                    )}
+
+
+                 </div>
+
+
+                </>
+
+
+        )}
+            
+         </div>
         </>
     )
 }
