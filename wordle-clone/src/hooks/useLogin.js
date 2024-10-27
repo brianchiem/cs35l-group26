db8f61976fbea536e9e69e4646e2d687810e289f
Brian Chiem <44745031+brianchiem@users.noreply.github.com>
2024-10-26 17:45:11 -0700

    Update useLogin.js

--- wordle-clone/src/hooks/useLogin.js
+++ wordle-clone/src/hooks/useLogin.js
@@ -13,7 +13,7 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('https://cs35l-group26.onrender.com/api/user/login', {
+        const response = await fetch(`${rootUrl}/user/login`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({identifier, password})
